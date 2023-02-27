25f68e2749184b3d73883b4b04b009ea0b7df2fa
leandroalves <leandro.alves@lighthouseit.com.br>
2023-02-27 14:53:50 -0300

    fix/bug context in app,tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd/dist/core";
 import Board from "./components/board";
-import { CardsProvider } from "./contexts";
+import { AllCardsProvider } from "./contexts";
 
 function App() {
   return (
-    <CardsProvider>
+    <AllCardsProvider>
       <DndProvider backend={HTML5Backend}>
         <Board />
       </DndProvider>
-    </CardsProvider>
+    </AllCardsProvider>
   );
 }
 
